2cf1946583ae58f40b91cb857ba7099e4a3d52c3
Unknown <johnbuevich@yahoo.com>
2018-02-26 15:43:02 +0300

    added server http

---
+++ server_config.py
@@ -0,0 +1,8 @@
+
+SERVER_NAME = 'http_server'
+HOST = '127.0.0.1'
+PORT = 80
+LISTENERS = 1000
+REQ_SIZE = 1024
+ROOT = '/home/john/TP/technopark-http-server-master/http-test-suite/'
+CPU_NUMBER = 4
